5549b7c4b4c0339d331822acc7991cec5303097d
Nakk47 <pozz.vathhd@gmail.com>
2025-06-15 20:42:28 +0700

    first commmit

---
+++ back/src/repositories/sqlArticleRepository.js
@@ -0,0 +1,84 @@
+//
+//  This repository shall:
+//  - Connect to the database (using the pool provided by the database.js)
+// -  Perfrom the SQL querries to implement the bellow API
+//
+
+import { pool } from "../utils/database.js";
+
+
+// Get all articles
+export async function getArticles() {
+    try{
+        const [result] = await pool.query(
+            `SELECT a.*, j.name AS journalist_name, a.category
+            FROM articles a
+            JOIN journalists j ON a.journalistId = j.id`
+        );
+        return result;
+    } catch (err) {
+        console.log("Error fetching article: ", err);
+        throw err;
+    }
+}
+
+// Get one article by ID
+export async function getArticleById(id) {
+    try{
+        const [result] = await pool.query(
+            `SELECT a.*, j.name AS journalist_name, a.category
+            FROM articles a
+            JOIN journalists j ON a.journalistId = j.id
+            WHERE a.id = ?`, [id]
+        );
+        return(result);
+    } catch (err) {
+        console.log("Error fetching article: ", err);
+        throw err;
+    }
+}
+
+// Create a new article
+export async function createArticle(article) {
+    const { title, content, journalistId, category} = article;
+    try {
+        const [result] = await pool.query(
+            'INSERT INTO articles (title, content, journalistId, category) VALUES (?, ?, ?, ?)',
+            [title, content, journalistId, category]
+        );
+        return result;
+
+    } catch(err) {
+        console.log("Error fetching article: ", err);
+        throw err;
+    }
+}
+
+// Update an article by ID
+export async function updateArticle(id, updatedData) {
+    const {title, content, journalistId, category} = updatedData;
+
+    try{
+        const [result] = await pool.query(
+            'UPDATE articles SET title = ?, content = ?, journalistId = ?, category = ? WHERE id = ?', 
+            [title, content, journalistId, category, id]);
+        return result.affectedRows > 0;
+    } catch (err) {
+        console.log("Error updating article: ", err);
+        throw err;
+    }
+}
+
+// Delete an article by ID
+export async function deleteArticle(id) {
+    try {
+        const [result] = await pool.query(
+            'DELETE FROM articles WHERE id = ?',
+            [id]
+        );
+        return result.affectedRows > 0; 
+    } catch (err) {
+        console.log("Error updating article: ", err);
+        throw err;
+    }
+}
---
+++ front/src/components/ArticleForm.jsx
@@ -0,0 +1,108 @@
+import { useParams, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { getArticleById, createArticle, updateArticle } from "../services/api";
+
+export default function ArticleForm({ isEdit }) {
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({
+    title: "",
+    content: "",
+    journalistId: "",
+    category: "",
+  });
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    if (isEdit && id) {
+      fetchArticle(id);
+    }
+  }, []);
+
+  const fetchArticle = async (id) => {
+    setIsLoading(true);
+    setError("");
+    try {
+      const article = await getArticleById(id);
+      setFormData(article);
+    } catch (err) {
+      setError("Failed to load article. Please try again.");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, 
+      [name]: name === "journalistId" ? Number(value) : value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    setError("");
+
+    try {
+      if (isEdit) {
+        await updateArticle(id, formData);
+      } else {
+        await createArticle(formData);
+      }
+      navigate("/articles");
+    } catch (err) {
+      setError("Failed to submit article.");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <>
+      {isLoading && <p>Loading...</p>}
+      {error && <p style={{ color: "red" }}>{error}</p>}
+
+      <form className="article-form" onSubmit={handleSubmit}>
+        <h2>{isEdit ? "Edit Article" : "Create Article"}</h2>
+        <input
+          name="title"
+          value={formData.title}
+          onChange={handleChange}
+          placeholder="Title"
+          required
+        />
+        <br />
+        <textarea
+          name="content"
+          value={formData.content}
+          onChange={handleChange}
+          placeholder="Content"
+          required
+        />
+        <br />
+        <input
+          name="journalistId"
+          value={formData.journalistId}
+          onChange={handleChange}
+          placeholder="Journalist ID"
+          required
+        />
+        <br />
+        <input
+          name="category"
+          value={formData.category}
+          onChange={handleChange}
+          placeholder="Category ID"
+          required
+        />
+        <br />
+        <button className="main" type="submit">
+          {isEdit ? "Edit " : "Create"}
+        </button>
+      </form>
+    </>
+  );
+}
